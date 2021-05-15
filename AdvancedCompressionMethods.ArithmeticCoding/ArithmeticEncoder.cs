bf6a8e7dc6a4f9413ec2f1d8abcf01084e0ccf26
Mihai Luca <luca.mihai97@yahoo.com>
2021-05-15 08:01:30 +0300

    Small progress on ArithmeticEncoder

--- AdvancedCompressionMethods.ArithmeticCoding/ArithmeticEncoder.cs
+++ AdvancedCompressionMethods.ArithmeticCoding/ArithmeticEncoder.cs
@@ -1,4 +1,7 @@
-﻿using AdvancedCompressionMethods.ArithmeticCoding.Interfaces;
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdvancedCompressionMethods.ArithmeticCoding.Entities;
+using AdvancedCompressionMethods.ArithmeticCoding.Interfaces;
 using AdvancedCompressionMethods.FileOperations.Interfaces;
 
 namespace AdvancedCompressionMethods.ArithmeticCoding
@@ -7,6 +10,11 @@ namespace AdvancedCompressionMethods.ArithmeticCoding
     {
         private readonly IFileReader fileReader;
         private readonly IFileWriter fileWriter;
+
+        private SortedDictionary<char, ArithmeticCodingSymbol> symbolDictionary;
+        private uint Low = uint.MinValue;
+        private uint High = uint.MaxValue;
+        private ulong Range = 1;
 
         public ArithmeticEncoder(IFileReader fileReader, IFileWriter fileWriter)
         {
@@ -17,12 +25,57 @@ namespace AdvancedCompressionMethods.ArithmeticCoding
         public void EncodeFile(string sourceFilepath, string destinationFilepath)
         {
             fileReader.Open(sourceFilepath);
-            fileWriter.Open(destinationFilepath);
-
+            var sourceFileBytes = GetAllBytesFromSourceFile();
             fileReader.Close();
+
+            InitializeSymbolDictionaryFromBytes(sourceFileBytes);
+
+
+            fileWriter.Open(destinationFilepath);
             fileWriter.Close();
 
             throw new System.NotImplementedException();
+        }
+
+        private List<byte> GetAllBytesFromSourceFile()
+        {
+            var bytes = new List<byte>();
+
+            while (!fileReader.ReachedEndOfFile)
+            {
+                var value = fileReader.ReadBits(8);
+                bytes.Add((byte)value);
+            }
+
+            return bytes;
+        }
+
+        private void InitializeSymbolDictionaryFromBytes(List<byte> bytes)
+        {
+            symbolDictionary = new SortedDictionary<char, ArithmeticCodingSymbol>();
+
+            foreach (var byteValue in bytes)
+            {
+                var character = (char)byteValue;
+
+                if (symbolDictionary.TryGetValue(character, out var symbol))
+                {
+                    symbol.Count++;
+                }
+                else
+                {
+                    var newSymbol = new ArithmeticCodingSymbol {Value = character};
+                    symbolDictionary.Add(character, newSymbol);
+                }
+            }
+
+            for (var i = 1; i < symbolDictionary.Count; i++)
+            {
+                var symbol = symbolDictionary.ElementAt(i).Value;
+                var previousSymbol = symbolDictionary.ElementAt(i - 1).Value;
+
+                symbol.Sum = previousSymbol.Sum + symbol.Count;
+            }
         }
     }
 }
---
+++ AdvancedCompressionMethods.ArithmeticCoding/Entities/ArithmeticCodingSymbol.cs
@@ -0,0 +1,9 @@
+﻿namespace AdvancedCompressionMethods.ArithmeticCoding.Entities
+{
+    public class ArithmeticCodingSymbol
+    {
+        public ushort Value { get; set; }
+        public int Count { get; set; }
+        public int Sum { get; set; }
+    }
+}

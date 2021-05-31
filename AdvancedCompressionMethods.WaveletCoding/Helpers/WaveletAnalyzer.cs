9b061bf54fb28c3863d7b082f4c100609d21820d
Mihai Luca <luca.mihai97@yahoo.com>
2021-05-31 14:54:27 +0300

    Implement WaveletCoefficentsProvider

--- AdvancedCompressionMethods.WaveletCoding/Helpers/WaveletAnalyzer.cs
+++ AdvancedCompressionMethods.WaveletCoding/Helpers/WaveletAnalyzer.cs
@@ -69,34 +69,10 @@ namespace AdvancedCompressionMethods.WaveletCoding.Helpers
             return newValues;
         }
 
-        // TODO: Consider extracting into configuration file/s
         private void InitializeCoefficients()
         {
-            analysisLowCoefficients = new List<double>
-            {
-                0.026748757411,
-                -0.016864118443,
-                -0.078223266529,
-                0.266864118443,
-                0.602949018236,
-                0.266864118443,
-                -0.078223266529,
-                -0.016864118443,
-                0.026748757411
-            };
-
-            analysisHighCoefficients = new List<double>
-            {
-                0.000000000000,
-                0.091271763114,
-                -0.057543526229,
-                -0.591271763114,
-                1.115087052457,
-                -0.591271763114,
-                -0.057543526229,
-                0.091271763114,
-                0.000000000000
-            };
+            analysisLowCoefficients = WaveletCoefficientsProvider.GetAnalysisLowCoefficients();
+            analysisHighCoefficients = WaveletCoefficientsProvider.GetAnalysisHighCoefficients();
         }
     }
 }
---
+++ AdvancedCompressionMethods.WaveletCoding/Helpers/WaveletCoefficientsProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace AdvancedCompressionMethods.WaveletCoding.Helpers
+{
+    // TODO: Make this a service instead of static
+    public static class WaveletCoefficientsProvider
+    {
+        // TODO: Consider extracting into configuration file/s
+        public static List<double> GetAnalysisLowCoefficients()
+        {
+            return new List<double>
+            {
+                0.026748757411,
+                -0.016864118443,
+                -0.078223266529,
+                0.266864118443,
+                0.602949018236,
+                0.266864118443,
+                -0.078223266529,
+                -0.016864118443,
+                0.026748757411
+            };
+        }
+
+        // TODO: Consider extracting into configuration file/s
+        public static List<double> GetAnalysisHighCoefficients()
+        {
+            return new List<double>
+            {
+                0.000000000000,
+                0.091271763114,
+                -0.057543526229,
+                -0.591271763114,
+                1.115087052457,
+                -0.591271763114,
+                -0.057543526229,
+                0.091271763114,
+                0.000000000000
+            };
+        }
+    }
+}
